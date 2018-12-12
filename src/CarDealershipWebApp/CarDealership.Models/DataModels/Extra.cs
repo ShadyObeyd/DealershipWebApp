8cd7ab4e762d96a04efb194a7dfc60af7ae27a10
ShadyObeyd <obeyd.shady@gmail.com>
2018-12-12 18:18:28 +0200

    Changed collection from vehicle to vehicleExtra in Extra.cs

--- src/CarDealershipWebApp/CarDealership.Models/DataModels/Extra.cs
+++ src/CarDealershipWebApp/CarDealership.Models/DataModels/Extra.cs
@@ -1,12 +1,18 @@
-﻿using CarDealership.Models.DataModels.Vehicles;
+﻿using CarDealership.Models.DataModels.MappingTables.Vehicles;
 using System.Collections.Generic;
 
 namespace CarDealership.Models.DataModels
 {
     public class Extra : BaseModel
     {
+        public Extra()
+            : base()
+        {
+            this.Vehicles = new HashSet<VehicleExtra>();
+        }
+
         public string Name { get; set; }
 
-        public virtual ICollection<Vehicle> Vehicles { get; set; }
+        public virtual ICollection<VehicleExtra> Vehicles { get; set; }
     }
 }
