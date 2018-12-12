9f7d237146131bdba0595e4404821b8cc94e14c1
ShadyObeyd <obeyd.shady@gmail.com>
2018-12-12 19:03:02 +0200

    Added Rim.cs

---
+++ src/CarDealershipWebApp/CarDealership.Models/DataModels/Rims/Rim.cs
@@ -0,0 +1,15 @@
+﻿using CarDealership.Models.DataModels.Contracts;
+
+namespace CarDealership.Models.DataModels.Rims
+{
+    public class Rim : BaseModel, ISellable
+    {
+        public string CarMake { get; set; }
+
+        public string CarModel { get; set; }
+
+        public string RimMake { get; set; }
+
+        public int BoltsCount { get; set; }
+    }
+}
