260bac0ede88bc6aea0b867ba02b1c566da85176
ShadyObeyd <obeyd.shady@gmail.com>
2018-12-12 18:42:18 +0200

    Added Tyre.cs

---
+++ src/CarDealershipWebApp/CarDealership.Models/DataModels/Tyres/Tyre.cs
@@ -0,0 +1,11 @@
+﻿namespace CarDealership.Models.DataModels.Tyres
+{
+    public class Tyre : BaseModel
+    {
+        public string Make { get; set; }
+
+        public int SppedIndex { get; set; }
+
+        public int WeightIndex { get; set; }
+    }
+}
