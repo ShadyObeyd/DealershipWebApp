504439e553a59f8f1d86ef948146fece25240453
ShadyObeyd <obeyd.shady@gmail.com>
2018-12-12 16:55:33 +0200

    Added Car.cs file and CarCategory.cs file

---
+++ src/CarDealershipWebApp/CarDealership.Models/DataModels/Vehicles/Car.cs
@@ -0,0 +1,9 @@
+﻿using CarDealership.Models.DataModels.Vehicles.Enums;
+
+namespace CarDealership.Models.DataModels.Vehicles
+{
+    public class Car : Vehicle
+    {
+        public CarCategory Category { get; set; }
+    }
+}
---
+++ src/CarDealershipWebApp/CarDealership.Models/DataModels/Vehicles/Enums/CarCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace CarDealership.Models.DataModels.Vehicles.Enums
+{
+    public enum CarCategory
+    {
+        Saloon = 1,
+        OffRoad = 2,
+        Van = 3,
+        Estate = 4,
+        Sports = 5,
+        Cabriolet = 6
+    }
+}
