7bc93709c9dc0918822919c47aea793917e464fb
ShadyObeyd <obeyd.shady@gmail.com>
2018-12-12 18:11:36 +0200

    Added Part.cs and PartCategory.cs

---
+++ src/CarDealershipWebApp/CarDealership.Models/DataModels/Parts/Enums/PartCategory.cs
@@ -0,0 +1,15 @@
+﻿namespace CarDealership.Models.DataModels.Parts.Enums
+{
+    public enum PartCategory
+    {
+        All = 1,
+        LpgSystems = 2,
+        AudioOrVideo = 3,
+        FuelSystem = 4,
+        Engine = 5,
+        ElectricSystem = 6,
+        ExhaustingSystem = 7,
+        Interior = 8,
+        Transmission = 9
+    }
+}
---
+++ src/CarDealershipWebApp/CarDealership.Models/DataModels/Parts/Part.cs
@@ -0,0 +1,11 @@
+﻿using CarDealership.Models.DataModels.Parts.Enums;
+
+namespace CarDealership.Models.DataModels.Parts
+{
+    public class Part : BaseModel
+    {
+        public string Name { get; set; }
+
+        public PartCategory Category { get; set; }
+    }
+}
