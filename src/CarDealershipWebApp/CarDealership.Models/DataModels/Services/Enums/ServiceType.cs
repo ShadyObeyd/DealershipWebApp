1136bc2dcf0e257490ebaf9ef3779e9dd4035bb9
ShadyObeyd <obeyd.shady@gmail.com>
2018-12-12 19:26:05 +0200

    Added Service.cs and ServiceType.cs

---
+++ src/CarDealershipWebApp/CarDealership.Models/DataModels/Services/Enums/ServiceType.cs
@@ -0,0 +1,10 @@
+﻿namespace CarDealership.Models.DataModels.Services.Enums
+{
+    public enum ServiceType
+    {
+        RentACar = 1,
+        Administration = 2,
+        CarWash = 3,
+        Repair = 4
+    }
+}
---
+++ src/CarDealershipWebApp/CarDealership.Models/DataModels/Services/Service.cs
@@ -0,0 +1,14 @@
+﻿using CarDealership.Models.DataModels.Contracts;
+using CarDealership.Models.DataModels.Services.Enums;
+
+namespace CarDealership.Models.DataModels.Services
+{
+    public class Service : BaseModel, ISellable
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ServiceType ServiceType { get; set; }
+    }
+}
