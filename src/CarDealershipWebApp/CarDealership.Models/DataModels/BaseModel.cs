ceaf04ea4fb78e8b086e17ed4f766f542993fcd2
ShadyObeyd <obeyd.shady@gmail.com>
2018-12-12 15:54:14 +0200

    Added the BaseModel.cs class

---
+++ src/CarDealershipWebApp/CarDealership.Models/DataModels/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CarDealership.Models.DataModels
+{
+    public abstract class BaseModel
+    {
+        public BaseModel()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+    }
+}
